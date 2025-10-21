c7a499501f9614b2fdfbec33b9832b37808efee6
Olga Kruglova <olha.kruhlova@inetum.com>
2025-10-21 20:00:06 +0200

    add css animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ module.exports = {
                     dark: '#704b5b', // (112,75,91)
                     darkest: '#4f3040', // (79,48,64)
                 },
-                // Or you can name them semantically
                 primary: {
                     50: '#e8cbe0',
                     100: '#a86487',
@@ -21,17 +20,10 @@ module.exports = {
                 },
             },
             backgroundImage: {
-                // Gradient from dark to light (left to right)
                 'gradient-palette': 'linear-gradient(to right, #4f3040, #704b5b, #977173, #a86487, #e8cbe0)',
-
-                // Gradient from dark to light (top to bottom)
                 'gradient-palette-vertical': 'linear-gradient(to bottom, #4f3040, #704b5b, #977173, #a86487, #e8cbe0)',
-
-                // Gradient from dark to light (diagonal)
                 'gradient-palette-diagonal':
                     'linear-gradient(to bottom right, #4f3040, #704b5b, #977173, #a86487, #e8cbe0)',
-
-                // Simple two-color gradients
                 'gradient-dark-light': 'linear-gradient(to right, #4f3040, #e8cbe0)',
                 'gradient-dark-medium': 'linear-gradient(to right, #4f3040, #977173)',
             },
